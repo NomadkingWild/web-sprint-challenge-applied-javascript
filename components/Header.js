d57c21ab063a110a5fb704b7339f97c2f6654cff
NomadkingWild <67304919+NomadkingWild@users.noreply.github.com>
2020-08-07 11:41:32 -0400

    struggling with the syntaxes again maybe im missing something?.

--- components/Header.js
+++ components/Header.js
@@ -10,5 +10,29 @@
 //
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
+const head = document.querySelector('.header-container')
+function Header() {
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const title = document.createElement('h1')
+    const temp = document.createElement('span')
 
-function Header() {}
+    header.appendChild(date)
+    header.appendChild(title)
+    header.appendChild(temp)
+
+    header.className = 'header'
+    date.className = 'date'
+    temp.className = 'temp'
+
+    date.textContent = header.date
+    title.textContent= header.title
+    temp.textContent= header.temp
+
+    return Header;
+    
+}
+
+header.appendChild(head)
+  console.log(header)
+    
